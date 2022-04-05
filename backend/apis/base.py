cacc6b30691437d343c480d163b0dab6c68c30fc
RomanKroecker <oldman_1988@yahoo.de>
2022-04-05 12:07:47 +0200

    Imports structured

---
+++ backend/apis/base.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  5 12:02:49 2022
+
+@author: RomKey
+"""
+from fastapi import APIRouter
+
+
+from apis.version1 import route_users 
+
+
+api_router = APIRouter()
+
+api_router.include_router(route_users.router, prefix="/user",tags=["users"])
