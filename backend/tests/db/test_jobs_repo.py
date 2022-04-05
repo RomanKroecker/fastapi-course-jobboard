6e3912871a9008db9a950d371a8ed1625693c120
RomanKroecker <oldman_1988@yahoo.de>
2022-04-05 15:50:18 +0200

    test

---
+++ backend/tests/db/test_jobs_repo.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  5 13:34:47 2022
+
+@author: RomKey
+"""
+from sqlalchemy.orm import Session 
+
+from db.repository.jobs import create_new_job, retreive_job
+from schemas.jobs import JobCreate 
+from tests.utils.users import create_random_owner 
+
+
+
+def test_retreive_job_by_id(db_session:Session):
+    title = "test title"
+    company = "test company"
+    company_url = "testcomp.com"
+    location = "USA,NY"
+    description = "Foo Bar"
+    owner = create_random_owner(db=db_session)
+    job_schema = JobCreate(title=title, company=company, company_url=company_url, location=location, description=description)
+    job = create_new_job(job=job_schema, db=db_session, owner_id=owner.id)
+    retreived_job = retreive_job(id=job.id, db=db_session)
+    assert retreived_job.id == job.id 
+    assert retreived_job.title == "test title"
