60eab848749ad07799ef54754510e2e44c6c691e
RomanKroecker <oldman_1988@yahoo.de>
2022-04-06 10:49:37 +0200

    Job by id

--- backend/apis/version1/route_jobs.py
+++ backend/apis/version1/route_jobs.py
@@ -10,7 +10,7 @@ from sqlalchemy.orm import Session
 from db.session import get_db 
 from db.models.jobs import Job 
 from schemas.jobs import JobCreate, ShowJob
-from db.repository.jobs import create_new_job 
+from db.repository.jobs import create_new_job, retreive_job
 
 
 
@@ -24,3 +24,10 @@ def create_job(job:JobCreate, db:Session=Depends(get_db)):
     return job
 
 
+@router.get("/get/{id}", response_model=ShowJob)
+def retreive_job_by_id(id:int, db:Session = Depends(get_db)):
+    job = retreive_job(id=id, db=db)
+    if not job:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                      detail=f"Job with id {id} does not exist")
+    return job
